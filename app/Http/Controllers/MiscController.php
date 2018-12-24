dd0b31ac626be095cd04b23b9a47df3a1d179a38
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2018-12-24 11:38:12 +0200

    Added slack notification integration for new payments for test

--- app/Http/Controllers/MiscController.php
+++ app/Http/Controllers/MiscController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MiscController extends Controller
 {
-    public function terms(){
+    public function terms()
+    {
         return view('misc.terms');
+    }
+
+    public function newPaymentToSlack()
+    {
+        $input = @file_get_contents("php://input");
+
+        $payload = json_decode($input);
+
+        Log::channel('slack')->info('New payment', ['data' => $payload]);
     }
 }
